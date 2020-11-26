22e2aac800eaff22261aed61c6ddffca1e8b6d97
walker-finlay <walker.finlay@gmail.com>
2020-11-25 21:15:37 -0500

    cleaning up & debugging draw_line

--- tools.py
+++ tools.py
@@ -49,22 +49,33 @@ def discretize(obstacles: np.array, objectives: np.array, n=20, diagonal=False):
     # Rasterize cuboids
     for cuboid in cuboids: fillrectangle(grid, (cuboid[0], cuboid[1]), cuboid[5])
 
-    plot_obstacles(cuboids, cylinders, (rob, goal), shift=True, grid=grid, n=n)
+    plot_obstacles(cuboids, cylinders, (rob, goal), grid=grid, n=n)
 
 
 def center2corners(center, gamma):
+    "returns corners as 2x4 array"
     # In : ((x,y),gamma)
     # Out: ((x1,y1),(x2,y2),(x3,y3),(x4,y4))
     r = array([[cos(gamma), -sin(gamma)],
                [sin(gamma),  cos(gamma)]])
-    corners = np.add(r.dot(some_corners).T, center).T
-    return corners
+    return np.add(r.dot(some_corners).T, center).T
+
+def draw_line(grid, edge):
+    # TODO: Implement me!
+    (x1,y1),(x2,y2) = edge
+    slope = (y2-y1)/(x2-x1)
+    for x in np.arange(int(x1),int(x2)):
+        grid[x, x+int(slope)] = 1
 
 def fillrectangle(grid, center, gamma):
-    # TODO: Implement me!
-    # Convert center (x,y) and gamma to corners/line segments
-    corners = center2corners(center, gamma)
     return
+    # TODO: Implement me!
+    corners = center2corners(center, gamma)
+    edges = array([(corners[:,0],corners[:,1]),
+                (corners[:,2],corners[:,3]),
+                (corners[:,1],corners[:,2]),
+                (corners[:,3],corners[:,0])])
+    for edge in edges: draw_line(grid, edge)
 
 def fillcircle(grid, center, radius):
     "Set interior of circle to 1 in grid" # FIXME what if circle is outside grid?
@@ -94,26 +105,27 @@ def build_graph(n, heuristic=Manhattan):
                 if next[0] >= 0 and next[0] < n and next[1] >= 0 and next[1] < n :
                     G[current].append(next)
 
-def plot_grid(grid):
-    fig,ax = plt.subplots()
-    im = ax.imshow(grid)
-    plt.show()
-
 # Plotem ----------------------------------------------------------------------
-def plot_obstacles(cbds, cyl, objectives, shift=False, n=20, grid=None):
+def plot_obstacles(cbds, cyl, poi, n=20, grid=None):
     fig,ax = plt.subplots()
+    offset = 0
 
     if grid is not None:
         im = ax.imshow(grid.T)
+        offset = 0.5
     plt.gca().invert_xaxis()
+
+    testcorners = center2corners((cbds[0,0],cbds[0,1]), cbds[0,5])
+    plt.plot(testcorners[0]-offset,testcorners[1]-offset,'wo')
+    print(testcorners)
 
     if n < 30:
         plt.grid(which='both')
-        ax.set_xticks(np.arange(-0.5,n+1))
-        ax.set_yticks(np.arange(-0.5,n+1))
+        ax.set_xticks(np.arange(-offset,n+1))
+        ax.set_yticks(np.arange(-offset,n+1))
 
     for i in cyl: # -0.5 is to line it up with the imshow at low resolutions
-        ax.add_patch(plt.Circle(i-0.5, radius=cd/2, alpha=0.75))
+        ax.add_patch(plt.Circle(i-offset, radius=cd/2, alpha=0.75))
     for i in cbds:
         # coppelia gives us the center, pyplot wants the bottom left
         x = i[0]     # Initial position
@@ -123,14 +135,11 @@ def plot_obstacles(cbds, cyl, objectives, shift=False, n=20, grid=None):
                         [sin(gamma), cos(gamma)]]) # Rotation
         pt = array([(-rw/2), (-rh/2)]) # Bottom left of the rectangle
         new_pt = rotation.dot(pt) + array([x, y])
-        ax.add_patch(plt.Rectangle(new_pt-0.5, rw, rh, degrees(gamma)))
+        ax.add_patch(plt.Rectangle(new_pt-offset, rw, rh, degrees(gamma)))
 
-    rob, goal = objectives
-    plt.plot(*tuple(rob-0.5), 'ro')     # Omnirob
-    plt.plot(*tuple(goal-0.5), 'go')  # Goal 
+    rob, goal = poi
+    plt.plot(*tuple(rob-offset), 'ro')     # Omnirob
+    plt.plot(*tuple(goal-offset), 'go')  # Goal 
 
-    if shift == False:
-        plt.xlim(-10,10)
-        plt.ylim(-10,10)
     plt.gca().set_aspect("equal")
     plt.show(block=True)
